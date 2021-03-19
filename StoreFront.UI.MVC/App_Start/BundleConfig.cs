2ddead41ef13bd88795646d0c9ae223a1c0b0115
james479 <james.e.glover57@gmail.com>
2021-03-18 20:38:54 -0500

    bundled styles and scripts

--- StoreFront.UI.MVC/App_Start/BundleConfig.cs
+++ StoreFront.UI.MVC/App_Start/BundleConfig.cs
@@ -7,24 +7,52 @@ namespace StoreFront.UI.MVC
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            /* bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+             // Use the development version of Modernizr to develop with and learn from. Then, when you're
+             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                       "~/Scripts/bootstrap.js",
+                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+             bundles.Add(new StyleBundle("~/Content/css").Include(
+                       "~/Content/bootstrap.css",
+                       "~/Content/site.css"));*/
+
+            bundles.Add(new ScriptBundle("~/bundles/css").Include(
+                    "~/Content/css/bootstrap.min.css",
+                    "~/Content/css/font-awesome.min.css",
+                    "~/Content/css/elegant-icons.css",
+                    "~/Content/css/magnific-popup.css",
+                    "~/Content/css/nice-select.css",
+                    "~/Content/css/owl.carousel.min.css",
+                    "~/Content/css/slicknav.min.css",
+                    "~/Content/css/style.css.map",
+                    "~/Content/css/style.css"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                    "~/Content/js/jquery-3.3.1.min.js",
+                    "~/Content/js/bootstrap.min.js",
+                    "~/Content/js/jquery.nice-select.min.js",
+                    "~/Content/js/jquery.nicescroll.min.js",
+                    "~/Content/js/jquery.magnific-popup.min.js",
+                    "~/Content/js/jquery.countdown.min.js",
+                    "~/Content/js/jquery.slicknav.js",
+                    "~/Content/js/mixitup.min.js",
+                    "~/Content/js/owl.carousel.min.js"
+                    //"~/Content/js/main.js"
+
+                ));
+
+            BundleTable.EnableOptimizations = false;
         }
     }
 }
